7ecabe76a292445313c2bb8bb0684889927c420c
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-06-13 19:56:06 +0700

    add int main BinaryTree dan variabel while

--- BinarySearchTreeC++/BinarySearchTreeC++.cpp
+++ BinarySearchTreeC++/BinarySearchTreeC++.cpp
@@ -115,6 +115,18 @@ public:
 			cout << ptr->info << " ";
 		}
 	}
+};
 
+int main()
+{
+	BinaryTree obj;
+	while (true)
+	{
+		cout << "\nMenu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. Perform preorder traversal" << endl;
+		cout << "4. Perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "Enter your choice (1-5): ";
 
-};  
